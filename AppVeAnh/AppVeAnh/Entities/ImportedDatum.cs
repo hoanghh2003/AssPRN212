1e3727dcc30c8532fa4887a3b92cf2d661819a53
hoanghh2003 <118053929+hoanghh2003@users.noreply.github.com>
2024-07-10 16:51:57 +0700

    Add project files.

---
+++ AppVeAnh/AppVeAnh/Entities/ImportedDatum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppVeAnh.Entities;
+
+public partial class ImportedDatum
+{
+    public int Id { get; set; }
+
+    public string? Column1 { get; set; }
+
+    public string? Column2 { get; set; }
+
+    public string? Column3 { get; set; }
+}
