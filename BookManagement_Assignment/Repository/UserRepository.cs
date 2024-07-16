7b818ec1e2919bcdf78925a8a408ebf231cbe25a
Duc1810 <146954012+Duc1810@users.noreply.github.com>
2024-07-16 15:17:23 +0700

    finish/AccountUser

--- BookManagement_Assignment/Repository/UserRepository.cs
+++ BookManagement_Assignment/Repository/UserRepository.cs
@@ -17,5 +17,56 @@ namespace Repository
             _context = new BookManagementDbContext();
             return _context.UserAccounts.FirstOrDefault(b => b.Email == email && b.Password == password);
         }
+
+        public UserAccount GetOne(string email, string password)
+        {
+
+            _context = new BookManagementDbContext();
+            return _context.UserAccounts.FirstOrDefault(e => e.Email == email && e.Password == password);
+            // so sánh 2 cuỗi dùng == ,  bản chất là so giá trị thực sự từng  kí tự có == nhua hay ko 
+            // vì chuỗi là kiểu tham chiếu
+        }
+
+        public List<UserAccount> GetAll()
+        {
+            _context = new BookManagementDbContext();
+            return _context.UserAccounts.ToList();
+        }
+        public List<UserAccount> Search(string name, string email)
+        {
+            // kh quan tâm hoa thường ta chuyển về thường rồi so sánh
+            _context = new();
+            return _context.UserAccounts.Where(b =>
+            (string.IsNullOrEmpty(name) || b.FullName.ToLower().Contains(name.ToLower())) &&
+            (string.IsNullOrEmpty(email) || b.Email.ToLower().Contains(email.ToLower()))).ToList();
+        }
+        public void Delete(UserAccount user)
+        {
+            _context = new();
+
+            _context.Remove(user);
+            _context.SaveChanges();
+
+        }
+        public UserAccount GetTop1Menber()
+        {
+            _context = new();
+            return _context.UserAccounts
+                       .OrderByDescending(u => u.MemberId)
+                       .FirstOrDefault();
+        }
+        public void CreateUser(UserAccount user)
+        {
+            _context = new();
+            _context.UserAccounts.Add(user);
+            _context.SaveChanges(true);
+        }
+        public void UpdateUser(UserAccount user)
+        {
+
+            _context = new();
+            _context.UserAccounts.Update(user);
+            _context.SaveChanges(true);
+        }
     }
 }
--- BookManagement_Assignment/Service/UserService.cs
+++ BookManagement_Assignment/Service/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using Repository;
+using Repository.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,6 +52,42 @@ namespace Services
         public bool CanSearch(int role)
         {
             return role == 1 || role == 2;
+        }
+         public List<UserAccount> getAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public List<UserAccount> SearchBook(string name, string email)
+        {
+            return _repository.Search(name, email);
+        }
+        public void Delete (UserAccount account)
+        {
+            _repository.Delete(account);
+        }
+        public UserAccount GetTop1()
+        {
+            return _repository.GetTop1Menber();
+        }
+        
+
+        public void  CreateUser (UserAccount account)
+        {
+            _repository.CreateUser(account);
+        }
+        public void UpdateUser (UserAccount account)
+        {
+            _repository.UpdateUser(account);
+        }
+        public bool CheckEmail (string email)
+        {
+           List<UserAccount> list = _repository.GetAll().ToList();
+            foreach (UserAccount account in list)
+            {
+                if(account.Email.Equals(email)) return true;
+            }
+            return false;
         }
     }
 }
